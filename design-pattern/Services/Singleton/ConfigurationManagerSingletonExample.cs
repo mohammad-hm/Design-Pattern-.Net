aeeb0c24260991bc8b5c7195a7dc522cc17d20db
mohammad hajimohammadi <hajimohammadi@amnpardaz.com>
2024-12-23 11:35:18 +0330

    feat: update singleton samples

---
+++ design-pattern/Services/Singleton/ConfigurationManagerSingletonExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_pattern.Services.Singleton
+{
+    public class ConfigurationManagerSingletonExample
+    {
+        private static readonly Lazy<ConfigurationManagerSingletonExample> _instance =
+            new Lazy<ConfigurationManagerSingletonExample>(() => new ConfigurationManagerSingletonExample());
+
+        private readonly Dictionary<string, string> _settings;
+
+        private ConfigurationManagerSingletonExample()
+        {
+            _settings = new Dictionary<string, string>
+        {
+            { "AppTitle", "My Application" },
+            { "Version", "1.0.0" }
+        };
+        }
+
+        public static ConfigurationManagerSingletonExample Instance => _instance.Value;
+
+        public string GetSetting(string key)
+        {
+            return _settings.TryGetValue(key, out var value) ? value : "Key not found";
+        }
+    }
+}
+
